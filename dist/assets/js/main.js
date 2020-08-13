925c1fc6ecf712b06161a811a4d1d3b70032ab22
Mikhail Teslya <televonvea@gmail.com>
2020-08-13 20:02:07 +0700

    before mobile

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,5 +1,19 @@
 "use strict";
 
 $(document).ready(function () {
-  $(".slider").slick();
+  $(".slider").slick({
+    dots: true,
+    slidesToShow: 3,
+    responsive: [{
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 2
+      }
+    }, {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1
+      }
+    }]
+  });
 });
