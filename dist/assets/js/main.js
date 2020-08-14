49064cc02274f95769afe0cffac435763b6204e3
Mikhail Teslya <televonvea@gmail.com>
2020-08-14 18:05:51 +0700

    without preloader and animations

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -26,24 +26,27 @@ var menu = document.getElementsByClassName("main__menu")[0];
 burger.onclick = function (e) {
   this.classList.toggle("burger-active");
   menu.classList.toggle("main__menu-active");
-}; // $(document).ready(function () {
-//   $('a[href^="#"]').click(function () {
-//     var el = $(this).attr("href");
-//     $("body").animate(
-//       {
-//         scrollTop: $(el).offset().top,
-//       },
-//       2000
-//     );
-//     return false;
-//   });
-// });
+  document.body.classList.toggle("no-scroll");
+  document.html.classList.toggle("no-scroll");
+};
 
+menu.onclick = function (e) {
+  if (burger.classList.contains("burger-active")) {
+    this.classList.toggle("main__menu-active");
+    burger.classList.toggle("burger-active");
+    document.body.classList.toggle("no-scroll");
+    document.html.classList.toggle("no-scroll");
+  }
+};
 
 $("body").on("click", '[href*="#"]', function (e) {
-  var fixed_offset = 100;
+  var fixed_offset = 0;
   $("html,body").stop().animate({
     scrollTop: $(this.hash).offset().top - fixed_offset
   }, 1000);
   e.preventDefault();
 });
+
+window.onload = function () {
+  document.body.classList.add("body_visible");
+};
