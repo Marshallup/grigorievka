ea796b974187c25655d93c65d150e57d9b171d1d
Mikhail Teslya <televonvea@gmail.com>
2020-08-15 16:38:19 +0700

    without animations

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -23,21 +23,20 @@ $(document).ready(function () {
 var burger = document.getElementsByClassName("burger")[0];
 var menu = document.getElementsByClassName("main__menu")[0];
 
-burger.onclick = function (e) {
+burger.onclick = function () {
   this.classList.toggle("burger-active");
   menu.classList.toggle("main__menu-active");
-  document.body.classList.toggle("no-scroll");
-  document.html.classList.toggle("no-scroll");
+  document.body.classList.toggle("no-scroll"); // document.html.classList.toggle("no-scroll");
 };
 
-menu.onclick = function (e) {
+menu.onclick = function () {
   if (burger.classList.contains("burger-active")) {
     this.classList.toggle("main__menu-active");
     burger.classList.toggle("burger-active");
-    document.body.classList.toggle("no-scroll");
-    document.html.classList.toggle("no-scroll");
+    document.body.classList.toggle("no-scroll"); // document.html.classList.toggle("no-scroll");
   }
-};
+}; // ПЛАВНЫЙ ПЕРЕХОД ПО ССЫЛКАМ
+
 
 $("body").on("click", '[href*="#"]', function (e) {
   var fixed_offset = 0;
@@ -45,8 +44,79 @@ $("body").on("click", '[href*="#"]', function (e) {
     scrollTop: $(this.hash).offset().top - fixed_offset
   }, 1000);
   e.preventDefault();
-});
+}); // ПРЕЛОАДЕР
 
 window.onload = function () {
-  document.body.classList.add("body_visible");
-};
+  document.querySelector(".preloader__wrap").classList.add("preloader__hide");
+  setTimeout(function () {
+    document.querySelector(".preloader__wrap").remove();
+  }, 800); // document.body.classList.add("body_hide");
+  // setTimeout(function () {
+  //   document.body.classList.add("body_visible");
+  // }, 1000);
+}; //  АНИМАЦИИ ПРИ СКРОЛЛЕ
+
+
+var animItems = document.querySelectorAll(".anim-items");
+
+if (animItems.length > 0) {
+  var animOnScroll = function animOnScroll() {
+    for (var i = 0; i < animItems.length; i++) {
+      var animItem = animItems[i];
+      var animItemHeight = animItem.offsetHeight;
+      var animItemOffset = offset(animItem).top;
+      var animStart = 4;
+      var animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (pageYOffset > animItemOffset - animItemPoint && pageYOffset < animItemOffset + animItemHeight) {
+        animItem.classList.add("anim-active");
+      } else {
+        animItem.classList.remove("anim-active");
+      }
+    }
+  };
+
+  window.addEventListener("scroll", animOnScroll);
+}
+
+function offset(el) {
+  var rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return {
+    top: rect.top + scrollTop,
+    left: rect.left + scrollLeft
+  };
+} // ПАРАЛАКС ЭФФЕКТ ДЛЯ ЛОГОТИПА НА ГЛАВНОМ ЭКРАНЕ
+// $(document).ready(function () {
+//   var banner = $(".banner");
+//   var imgs = $(".img");
+//   // /////////////////////////////////////////////////////////////
+//   function showAllObjects(object) {
+//     object.fadeIn(900);
+//   }
+//   // /////////////////////////////////////////////////////////////
+//   function moving(object, speed) {
+//     banner.on("mousemove", function (event) {
+//       var X = Math.floor(event.pageX / speed - 20) + "px";
+//       var Y = Math.floor(event.pageY / speed) + "px";
+//       object.css("transform", "translate(" + X + " , " + Y + ")");
+//     });
+//   }
+//   // /////////////////////////////////////////////////////////////
+//   function moveAll(object) {
+//     moving($(object[0]), 110);
+//     moving($(object[1]), 22);
+//     // moving($(object[2]), 8);
+//     // moving($(object[3]), 10);
+//     // moving($(object[4]), 20);
+//   }
+//   // /////////////////////////////////////////////////////////////
+//   showAllObjects(imgs);
+//   moveAll(imgs);
+//   // /////////////////////////////////////////////////////////////
+// });
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -26,21 +26,23 @@ $(document).ready(function () {
 let burger = document.getElementsByClassName("burger")[0];
 let menu = document.getElementsByClassName("main__menu")[0];
 
-burger.onclick = function (e) {
+burger.onclick = function () {
   this.classList.toggle("burger-active");
   menu.classList.toggle("main__menu-active");
   document.body.classList.toggle("no-scroll");
-  document.html.classList.toggle("no-scroll");
+  // document.html.classList.toggle("no-scroll");
 };
 
-menu.onclick = function (e) {
+menu.onclick = function () {
   if (burger.classList.contains("burger-active")) {
     this.classList.toggle("main__menu-active");
     burger.classList.toggle("burger-active");
     document.body.classList.toggle("no-scroll");
-    document.html.classList.toggle("no-scroll");
+    // document.html.classList.toggle("no-scroll");
   }
 };
+
+// ПЛАВНЫЙ ПЕРЕХОД ПО ССЫЛКАМ
 
 $("body").on("click", '[href*="#"]', function (e) {
   var fixed_offset = 0;
@@ -50,6 +52,83 @@ $("body").on("click", '[href*="#"]', function (e) {
   e.preventDefault();
 });
 
+// ПРЕЛОАДЕР
+
 window.onload = () => {
-  document.body.classList.add("body_visible");
+  document.querySelector(".preloader__wrap").classList.add("preloader__hide");
+  setTimeout(function () {
+    document.querySelector(".preloader__wrap").remove();
+  }, 800);
+  // document.body.classList.add("body_hide");
+  // setTimeout(function () {
+  //   document.body.classList.add("body_visible");
+  // }, 1000);
 };
+
+//  АНИМАЦИИ ПРИ СКРОЛЛЕ
+
+let animItems = document.querySelectorAll(".anim-items");
+
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll() {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("anim-active");
+      } else {
+        animItem.classList.remove("anim-active");
+      }
+    }
+  }
+}
+
+function offset(el) {
+  const rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+}
+
+// ПАРАЛАКС ЭФФЕКТ ДЛЯ ЛОГОТИПА НА ГЛАВНОМ ЭКРАНЕ
+
+// $(document).ready(function () {
+//   var banner = $(".banner");
+//   var imgs = $(".img");
+//   // /////////////////////////////////////////////////////////////
+//   function showAllObjects(object) {
+//     object.fadeIn(900);
+//   }
+//   // /////////////////////////////////////////////////////////////
+//   function moving(object, speed) {
+//     banner.on("mousemove", function (event) {
+//       var X = Math.floor(event.pageX / speed - 20) + "px";
+//       var Y = Math.floor(event.pageY / speed) + "px";
+//       object.css("transform", "translate(" + X + " , " + Y + ")");
+//     });
+//   }
+//   // /////////////////////////////////////////////////////////////
+//   function moveAll(object) {
+//     moving($(object[0]), 110);
+//     moving($(object[1]), 22);
+//     // moving($(object[2]), 8);
+//     // moving($(object[3]), 10);
+//     // moving($(object[4]), 20);
+//   }
+//   // /////////////////////////////////////////////////////////////
+//   showAllObjects(imgs);
+//   moveAll(imgs);
+//   // /////////////////////////////////////////////////////////////
+// });
